1173246e3a6639d8dfc1b76d989b02b731f59a09
Sp1302 <43338163+Sp1302@users.noreply.github.com>
2019-02-19 12:18:35 +0530

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,15 @@
+# SET-2
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,sum=0,i;
+printf("\nEnter the number");
+scanf("%d",&n);
+for(i=1;i<=5;i++)
+{
+sum=i*n;
+printf("%d\n",sum);
+}
+getch();
+}
